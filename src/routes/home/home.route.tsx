0640e2785f92c87aaa944bcb08399bb1b1597cbf
smrazvan <razvanro666@gmail.com>
2022-11-05 23:49:52 +0200

    dynamically load drawer options

--- src/routes/home/home.route.tsx
+++ src/routes/home/home.route.tsx
@@ -1,16 +1,16 @@
-import { Avatar, Button, Card, CardActions, CardContent, CardMedia, Chip, Stack, Typography } from "@mui/material";
+import { Avatar, Box, Button, Card, CardActions, CardContent, CardMedia, Chip, Stack, Typography } from "@mui/material";
 import React from "react";
 import { BlogCard } from "../../components/blog-card/blog-card.component";
 export const Home = () => {
   const tags:string[] = ["C#"];
   return (
-    <>
+    <Box sx={{display: "flex", flexWrap: "wrap", gap: 4, justifyContent: "center"}}>
       <BlogCard tags={tags} name="Ionel Marcel" title="Interfaces in C#" img="https://code.visualstudio.com/assets/docs/languages/csharp/c_sharp_hero.png" shortDescription="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis nam blanditiis et corrupti, dolorem esse commodi inventore, culpa obcaecati velit, sit molestiae reprehenderit dolorum dolore earum deleniti optio non expedita?" />
       <BlogCard tags={tags} name="Ionel Marcel" title="Interfaces in C#" img="https://code.visualstudio.com/assets/docs/languages/csharp/c_sharp_hero.png" shortDescription="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis nam blanditiis et corrupti, dolorem esse commodi inventore, culpa obcaecati velit, sit molestiae reprehenderit dolorum dolore earum deleniti optio non expedita?" />
       <BlogCard tags={tags} name="Ionel Marcel" title="Interfaces in C#" img="https://code.visualstudio.com/assets/docs/languages/csharp/c_sharp_hero.png" shortDescription="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis nam blanditiis et corrupti, dolorem esse commodi inventore, culpa obcaecati velit, sit molestiae reprehenderit dolorum dolore earum deleniti optio non expedita?" />
       <BlogCard tags={tags} name="Ionel Marcel" title="Interfaces in C#" img="https://code.visualstudio.com/assets/docs/languages/csharp/c_sharp_hero.png" shortDescription="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis nam blanditiis et corrupti, dolorem esse commodi inventore, culpa obcaecati velit, sit molestiae reprehenderit dolorum dolore earum deleniti optio non expedita?" />
       <BlogCard tags={tags} name="Ionel Marcel" title="Interfaces in C#" img="https://code.visualstudio.com/assets/docs/languages/csharp/c_sharp_hero.png" shortDescription="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis nam blanditiis et corrupti, dolorem esse commodi inventore, culpa obcaecati velit, sit molestiae reprehenderit dolorum dolore earum deleniti optio non expedita?" />
       <BlogCard tags={tags} name="Ionel Marcel" title="Interfaces in C#" img="https://code.visualstudio.com/assets/docs/languages/csharp/c_sharp_hero.png" shortDescription="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis nam blanditiis et corrupti, dolorem esse commodi inventore, culpa obcaecati velit, sit molestiae reprehenderit dolorum dolore earum deleniti optio non expedita?" />
-    </>
+    </Box>
   );
 }
